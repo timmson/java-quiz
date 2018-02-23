7e481516b87c3fad9a0f10def879c4201f2e1d1c
timmson <timmson666@mail.ru>
2018-02-23 19:02:19 +0300

    Init

--- src/main/java/ru/raiffeisen/javaschool/Q9.java
+++ src/main/java/ru/raiffeisen/javaschool/Q9.java
@@ -4,7 +4,8 @@ public class Q9 {
 
     public static void main(String[] args) {
         // \u0022 is the Unicode escape for double quote (")
-        System.out.println("a\u0022.length() + \u0022b".length());
+        int i = "a\u0022.length() + \u0022b".length();
+        System.out.println(i);
 
     }
 
